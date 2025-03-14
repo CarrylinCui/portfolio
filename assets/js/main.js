2673ecb42dc4aa250b5ef4507baa0292e650e2b0
CarrylinCui <carrylin.cui@gmail.com>
2025-03-14 09:17:36 +0900

    contact me button js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,4 @@
 
 function btnClick() {
     alert('Still completing the function now, please email or text me.\nThank you very much.')
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> fcbecc6f6482b9aed79d5e51d5d756a30d936424
